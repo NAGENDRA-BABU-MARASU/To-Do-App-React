1949dc5394c71120d56567db32c1a15a19065b85
Nagendra <nagendrababu.marasu@gmail.com>
2024-05-01 12:15:06 +0530

    checkpoint:4 added Empty button to clear all todo items

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import ToDoItem from './ToDoItem';
 
 export default function ToDoList() {
@@ -8,11 +9,29 @@ export default function ToDoList() {
 		'Sleep for 2 hours',
 		'Take a shower',
 	];
+	const [todoItems, setTodoItems] = useState(toDoListItems);
+	const noItemsText = 'Nothing to do buddy. Sleep!';
+
+	const clearAllToDoItems = () => {
+		setTodoItems([]);
+	};
+
+	const getToDoItems = () => {
+		return todoItems.map((toDoItem) => <ToDoItem key={toDoItem} task={toDoItem} />);
+	};
+
+	const noItemsElement = () => {
+		return <i>{noItemsText} </i>;
+	};
+
 	return (
-		<div className="ToDoList">
-			{toDoListItems.map((toDoItem) => (
-				<ToDoItem key={toDoItem} task={toDoItem} />
-			))}
-		</div>
+		<>
+			<div className="ToDoList">{todoItems.length > 0 ? getToDoItems() : noItemsElement()}</div>
+			<div>
+				<button className="ClearAllBtn" onClick={clearAllToDoItems}>
+					Empty
+				</button>
+			</div>
+		</>
 	);
 }
